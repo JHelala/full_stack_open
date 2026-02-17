945ecf6714a0b715bbc6edc1b66b85492a976d57
JHelala <juuso.helala@edu.turkuamk.fi>
2026-02-17 14:05:27 +0200

    1.6 - 1.8 done

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+
+const Statistics = (props) => {
+  const { good, neutral, bad } = props
+  const all = good + neutral + bad
+  const average = (good - bad) / all
+  const positive = (good / all) * 100
+
+  return (
+    <div>
+      <h1>Statistics</h1>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>All: {all}</p>
+      <p>Average: {average.toFixed(2)}</p>
+      <p>Positive: {positive.toFixed(2)} %</p>
+    </div>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const all = good + neutral + bad
+  const average = (good - bad) / all
+  const positive = (good / all) * 100
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+
+      {/* Would need to add a thing so that you can't give multiple feedbacks if put to proper use */}
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+
+      <Statistics good={good} neutral={neutral} bad={bad} all={all} average={average} positive={positive} />
+    </div>
+  )
+}
+
+export default App
